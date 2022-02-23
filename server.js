e028cd253fbd04762b540a800cf2c6f2280e28a8
wyork <w.york63@gmail.com>
2022-02-23 17:42:12 -0500

    fixed issue can now login

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ const sess = {
 };
 
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,7 +34,7 @@ app.set('view engine', 'handlebars');
 // turn on routes
 app.use(routes);
 
-app.use(session(sess));
+
 
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
